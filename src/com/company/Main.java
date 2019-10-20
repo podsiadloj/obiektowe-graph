e43a6e3ef9e1ac0bd14285ebebc3ba90c74e3888
Jan Podsiadło <janpodsiadlo95@gmail.com>
2019-10-20 23:47:58 +0200

    Implement Reader

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,26 @@
 package com.company;
 
+import jdk.jshell.spi.ExecutionControl;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionControl.NotImplementedException {
 	// write your code here
+        String input = "0110\n" +
+            "0001 0010 0010 0011\n" +
+            "0010 0001 0100\n" +
+            "0011 0010 0101 0110\n" +
+            "0100 0001 0110\n" +
+            "0101 0000\n" +
+            "0110 0000\n" +
+            "\n" +
+            "t(1x4)eks(2x2)t\n" +
+            "t(1x3)e(1x2)ks(3x3)t(1x2)\n" +
+            "przy(2x6)kl(1x10)ad\n" +
+            "ksia(1x2)zka\n" +
+            "rez(3x5)erw(2x10)ac(3x3)ja(1x2)\n" +
+            "dow(2x2)od(1x2)\n";
+        Reader reader = new ReaderImpl();
+        reader.read(input);
     }
 }
---
+++ src/com/company/ReaderImpl.java
@@ -0,0 +1,65 @@
+package com.company;
+
+import jdk.jshell.spi.ExecutionControl;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.regex.MatchResult;
+import java.util.regex.Pattern;
+
+public class ReaderImpl implements Reader {
+    public Graph read(String input) {
+        String[] lines = input.split("\\r?\\n");
+        int verticesCount = Integer.parseInt(lines[0], 2);
+        String[] verticesStructureDesc = Arrays.copyOfRange(lines, 1, verticesCount + 1);
+        String[] verticesContentDesc = Arrays.copyOfRange(lines, verticesCount + 2, lines.length);
+        System.out.println(Arrays.asList(verticesStructureDesc));
+        System.out.println(Arrays.asList(verticesContentDesc));
+        Vertex[] vertices = Arrays.stream(verticesContentDesc)
+                .map(this::parseVertex)
+                .toArray(Vertex[]::new);
+        ArrayList<Edge> edges = new ArrayList<>();
+        for (int i = 0; i < verticesCount; i++) {
+            Integer[] vertexDigits = Arrays.stream(verticesStructureDesc[i].split(" "))
+                    .map(s -> Integer.parseInt(s, 2))
+                    .toArray(Integer[]::new);
+            Integer[] ends = Arrays.copyOfRange(vertexDigits, 2, vertexDigits.length);
+            for (Integer end: ends){
+                edges.add(new Edge(vertices[i], vertices[end-1]));
+                System.out.println("begin: " + (i + 1) + ", end: " + end);
+            }
+        }
+        return new Graph(new ArrayList<>(Arrays.asList(vertices)), edges);
+    }
+
+    private Vertex parseVertex(String coded){
+        String content = "";
+        String regex = "[(][0-9]+x[0-9]+[)]";
+        String[] texts = coded.split(regex);
+        String[] repeaters = Pattern.compile(regex)
+                .matcher(coded)
+                .results()
+                .map(MatchResult::group)
+                .toArray(String[]::new);
+        for (int i = 0; i < texts.length; i++) {
+            content += texts[i];
+            if(i<repeaters.length){
+                content += repeat(content, repeaters[i]);
+            }
+        }
+        System.out.println(content);
+        return new Vertex(content);
+    }
+
+    private String repeat(String input, String repeater){
+        String[] cleanRepeater = repeater.replaceAll("[()]", "").split("x");
+        int fragmentLength = Math.min(input.length(), Integer.parseInt(cleanRepeater[0]));
+        int repeats = Integer.parseInt(cleanRepeater[1]);
+        String fragment = input.substring(input.length() - fragmentLength);
+        String result = "";
+        for (int i = 0; i < repeats - 1; i++) {
+            result += fragment;
+        }
+        return result;
+    }
+}
